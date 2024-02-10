3e14964cbfacd91ae3e76a24f28fbf20743069de
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-10 17:01:14 -0500

    create a component

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, TextInput, View, Text } from 'react-native';
+
+const LoginForm = () => {
+	return (
+		<View>
+			<Text>LoginForm Component</Text>
+		</View>
+	);
+};
+
+export default LoginForm;
+
+const styles = StyleSheet.create({});
